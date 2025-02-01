d38bc45a08b20952d09527d9c63afa3bc8a0404f
Mohammad Yaser Azrak <yaserazrak5@gmail.com>
2025-02-01 22:25:15 +0400

    Update "Contains Duplicate" solution

--- ArraysAndHashing/contains_duplicate.py
+++ ArraysAndHashing/contains_duplicate.py
@@ -1,7 +1,45 @@
 from collections import Counter
-from typing import List
 
 
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        return any(num != 1 for num in Counter(nums).values())
+# Using Python features
+def containsDuplicate(nums):
+    return any(num != 1 for num in Counter(nums).values())
+
+
+# Brute force
+# Time: O(n^2)
+# Space: O(1)
+def containsDuplicate(nums):
+    n = len(nums)
+    for i in range(n - 1):
+        for j in range(i + 1, n):
+            if nums[i] == nums[j]:
+                return True
+    return False
+
+
+# With sorting
+# Time: O(nlogn) which is the cost of sorting
+# Space: O(1)
+def containsDuplicate(nums):
+    n = len(nums)
+    nums.sort()
+    for i in range(0, n - 1):
+        if nums[i] == nums[i + 1]:
+            return True
+    return False
+
+
+# With a hash set
+# Time: O(n)
+# Space: O(n)
+def containsDuplicate(nums):
+    hash_set = set()
+    for n in nums:
+        if n in hash_set:
+            return True
+        hash_set.add(n)
+    return False
+
+
+print(containsDuplicate([1, 1, 2, 3, 4, 5, 6]))
