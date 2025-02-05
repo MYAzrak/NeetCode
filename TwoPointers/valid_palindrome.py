99245d7453e0952306ad39d20ab06db122482622
Mohammad Yaser Azrak <yaserazrak5@gmail.com>
2025-02-05 22:44:35 +0400

    Update "Valid Palindrome" solution

--- TwoPointers/valid_palindrome.py
+++ TwoPointers/valid_palindrome.py
@@ -1,3 +1,4 @@
+# Using reverse
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         s = s.lower()
@@ -5,30 +6,30 @@ class Solution:
         return s == s[::-1]
 
 
-test = Solution()
-print(test.isPalindrome("race a car"))
-print(test.isPalindrome("racecar"))
+# Two pointers
+# Time: O(n)
+# Space: O(1)
+def isPalindrome(s: str) -> bool:
+    start_ptr = 0
+    end_ptr = len(s) - 1
+    while start_ptr < end_ptr:
+        if not s[start_ptr].isalnum():
+            start_ptr += 1
+            continue
+
+        if not s[end_ptr].isalnum():
+            end_ptr -= 1
+            continue
+
+        if s[start_ptr].lower() != s[end_ptr].lower():
+            return False
+        else:
+            start_ptr += 1
+            end_ptr -= 1
+    return True
 
 
-# NeetCode solution for better space complexity and without using isalnum()
-# class Solution:
-#     def isPalindrome(self, s: str) -> bool:
-#         l, r = 0, len(s) - 1
-#         while l < r:
-#             while l < r and not self.alphanum(s[l]):
-#                 l += 1
-#             while l < r and not self.alphanum(s[r]):
-#                 r -= 1
-#             if s[l].lower() != s[r].lower():
-#                 return False
-#             l += 1
-#             r -= 1
-#         return True
-
-#     # Could write own alpha-numeric function
-#     def alphanum(self, c):
-#         return (
-#             ord("A") <= ord(c) <= ord("Z")
-#             or ord("a") <= ord(c) <= ord("z")
-#             or ord("0") <= ord(c) <= ord("9")
-#         )
+print(isPalindrome("race a car"))
+print(isPalindrome("racecar"))
+print(isPalindrome(" "))
+print(isPalindrome("A man, a plan, a canal: Panama"))
