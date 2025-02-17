e63c418505a671e381c35ad9301ca3844092a194
Mohammad Azrak <yaserazrak5@gmail.com>
2025-02-17 21:33:31 +0400

    Update "Best Time to Buy and Sell Stock" solution

--- SlidingWindow/best_time_to_buy_and_sell_stock.py
+++ SlidingWindow/best_time_to_buy_and_sell_stock.py
@@ -1,29 +1,18 @@
-class Solution:
-    def maxProfit(self, prices) -> int:
-        buy_ptr, sell_ptr = 0, 1
-        max_profit = 0
-        while sell_ptr < len(prices):
-            if prices[sell_ptr] > prices[buy_ptr]:
-                profit = prices[sell_ptr] - prices[buy_ptr]
-                max_profit = max(profit, max_profit)
-            else:
-                buy_ptr = sell_ptr
-            sell_ptr += 1
-        return max_profit
+# Two pointers
+# Time: O(N)
+# Space: O(1)
+def maxProfit(prices) -> int:
+    buy_ptr, sell_ptr = 0, 1
+    max_profit = 0
+    while sell_ptr < len(prices):
+        if prices[sell_ptr] > prices[buy_ptr]:
+            profit = prices[sell_ptr] - prices[buy_ptr]
+            max_profit = max(profit, max_profit)
+        else:
+            buy_ptr = sell_ptr  # go to the min price
+        sell_ptr += 1
+    return max_profit
 
 
-test = Solution()
-# print(test.maxProfit([1, 2, 4, 2, 5, 7, 2, 4, 9, 0]))
-
-# A unique solution I found on LeetCode
-# class Solution:
-#     def maxProfit(self, prices: List[int]) -> int:
-
-#         min_price = float('inf')
-#         profit = 0
-#         for price in prices:
-#             if price < min_price:
-#                 min_price = price
-#             elif  price - min_price > profit:
-#                 profit = price - min_price
-#         return profit
+print(maxProfit(prices=[7, 1, 5, 3, 6, 4]))
+print(maxProfit(prices=[7, 6, 4, 3, 1]))
