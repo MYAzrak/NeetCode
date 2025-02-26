54be89cc0c1cfdf9971d9ccd981fde5db2783861
Mohammad Yaser Azrak <yaserazrak5@gmail.com>
2025-02-26 18:12:46 +0400

    Update "Binary Search" solution

--- BinarySearch/binary_search.py
+++ BinarySearch/binary_search.py
@@ -1,21 +1,18 @@
-from typing import List
+# Binary search
+# Time: O(log n) since we dividing the solution space by 2 each time 
+# Space: O(1)
+def search(nums, target: int) -> int:
+    left, right = 0, len(nums) - 1
+    while left <= right:
+        # mid = left + (right - left) // 2 # You can use this to prevent overflow in languages that have overflow
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
 
 
-class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        left, right = 0, len(nums) - 1
-        while left <= right:
-            # mid = left + (right - left) // 2 # You can use this to prevent overflow in languages that have overflow
-            mid = (left + right) // 2
-            if nums[mid] == target:
-                return mid
-            elif nums[mid] < target:
-                left = mid + 1
-            else:
-                right = mid - 1
-        return -1
-
-
-test = Solution()
-print(test.search([-1, 0, 3, 5, 9, 12], 9))
-print(test.search([-1, 0, 3, 5, 9, 12], 11))
+print(search(nums=[-1, 0, 3, 5, 9, 12], target=9))
