f49edc7cf26d3b8dc9ac774bfbb92e4d913e392d
Mohammad Yaser Azrak <yaserazrak5@gmail.com>
2025-02-27 14:20:35 +0400

    Update "Linked List Cycle" solution

--- LinkedList/linked_list_cycle.py
+++ LinkedList/linked_list_cycle.py
@@ -1,37 +1,34 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 
-# Time: O(n). Space: O(n)
-class Solution:
-    def hasCycle(self, head) -> bool:
-        if not head or not head.next:
-            return False
+# Using a hash set
+# Time: O(n)
+# Space: O(n)
+def hasCycle(head) -> bool:
+    hash_set = set()
+    while head:
+        if head in hash_set:
+            return True
+        hash_set.add(head)
+        head = head.next
 
-        my_hash = set()
-        while head.next:
-            if head in my_hash:
-                return True
-            my_hash.add(head)
-            head = head.next
-
-        return False
+    return False
 
 
-# Floyd's Tortoise and Hare. Time: O(n). Space: O(1)
-class Solution:
-    def hasCycle(self, head) -> bool:
-        if not head or not head.next:
-            return False
-        slow, fast = head, head
+# Floyd's Tortoise and Hare.
+# Time: O(n)
+# Space: O(1)
+def hasCycle(head) -> bool:
+    slow, fast = head, head
 
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-            if slow == fast:
-                return True
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:
+            return True
 
-        return False
+    return False
