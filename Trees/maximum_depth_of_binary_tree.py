956e162e1bfeb471924daab4574bd982f9e6d140
Mohammad Yaser Azrak <yaserazrak5@gmail.com>
2025-03-03 17:50:55 +0400

    Update "Maximum Depth of Binary Tree" solution

--- Trees/maximum_depth_of_binary_tree.py
+++ Trees/maximum_depth_of_binary_tree.py
@@ -1,47 +1,58 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 
 from collections import deque
 
 
-class Solution:
-    # Recursive DFS: O(n), O(n)
-    # def maxDepth(self, root) -> int:
-    #     if not root:
-    #         return 0
-    #     return 1 + max(self.maxDepth(root.right), self.maxDepth(root.left))
+# Recursive DFS:
+# Time: O(n)
+# Space: O(n)
+def maxDepth(root) -> int:
+    if not root:
+        return 0
+    return 1 + max(maxDepth(root.right), maxDepth(root.left))
 
-    # Iterative BFS (level by level examination): O(n), O(n)
-    # def maxDepth(self, root) -> int:
-    #     if not root:
-    #         return 0
 
-    #     level = 0
-    #     queue = deque([root])
-    #     while queue:
-    #         for _ in range(len(queue)):
-    #             node = queue.popleft()
-    #             if node.left:
-    #                 queue.append(node.left)
-    #             if node.right:
-    #                 queue.append(node.right)
-    #         level += 1
-    #     return level
+# Iterative DFS:
+# Time: O(n)
+# Space: O(n)
+def maxDepth(root) -> int:
+    if not root:
+        return 0
 
-    # Iterative DFS: O(n), O(n)
-    def maxDepth(self, root) -> int:
-        stack = [[root, 1]]
-        max_depth = 1
-        while stack:
-            node, depth = stack.pop()
+    call_stack = [[root, 1]]
+    max_depth = 0
 
-            if node:
-                max_depth = max(max_depth, depth)
-                stack.append([node.left, depth + 1])
-                stack.append([node.right, depth + 1])
+    while call_stack:
+        node, depth = call_stack.pop()
+        if node:
+            max_depth = max(max_depth, depth)
+            call_stack.append([node.left, depth + 1])
+            call_stack.append([node.right, depth + 1])
 
-        return max_depth
+    return max_depth
+
+
+# Iterative BFS (level by level examination)
+# Time: O(n)
+# Space: O(n)
+def maxDepth(root) -> int:
+    if not root:
+        return 0
+
+    level = 0
+    queue = deque([root])
+    while queue:
+        for _ in range(len(queue)):
+            node = queue.popleft()
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        level += 1
+    return level
