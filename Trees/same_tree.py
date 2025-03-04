743a6afcf7bbb84eedcf3bc42f751fcdbc23853b
Mohammad Yaser Azrak <yaserazrak5@gmail.com>
2025-03-04 14:47:29 +0400

    Update "Same Tree" solution

--- Trees/same_tree.py
+++ Trees/same_tree.py
@@ -5,46 +5,12 @@ class TreeNode:
         self.left = left
         self.right = right
 
-
-class Solution:
-    def isSameTree(self, p, q) -> bool:
-        if not p and not q:
-            return True
-        if not p or not q or p.val != q.val:
-            return False
-        return self.isSameTree(p.right, q.right) and self.isSameTree(p.left, q.left)
-    
-# class Solution:
-#     def isSameTree(self, p, q) -> bool:
-#         if not p and not q:
-#             return True
-#         elif not p and q:
-#             return False
-#         elif p and not q:
-#             return False
-#         elif p.val != q.val:
-#             return False
-#         my_list1 = []
-#         my_list2 = []
-#         my_list1.append(self.isSameTree(p.right, q.right))
-#         my_list2.append(self.isSameTree(p.left, q.left))
-#         if False in my_list1 or False in my_list2:
-#             return False
-#         return True
-
-
-test = Solution()
-p = TreeNode(1)
-p.left = TreeNode(2)
-p.right = TreeNode(3)
-
-q = TreeNode(1)
-q.left = TreeNode(2)
-q.right = TreeNode(3)
-
-# p = TreeNode(1)
-# p.left = TreeNode(2)
-
-# q = TreeNode(1)
-# q.right = TreeNode(2)
-print(test.isSameTree(p, q))
+# DFS
+# Time: O(N)
+# Space: O(N)
+def isSameTree(self, p, q) -> bool:
+    if not p and not q:
+        return True
+    if not p or not q or p.val != q.val:
+        return False
+    return self.isSameTree(p.right, q.right) and self.isSameTree(p.left, q.left)
