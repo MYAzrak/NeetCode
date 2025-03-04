b33b8bb19ec1ea90984a4d0232aa71e713d20019
Mohammad Yaser Azrak <yaserazrak5@gmail.com>
2025-03-04 15:04:38 +0400

    Update "Subtree of Another Tree" solution

--- Trees/subtree_of_another_tree.py
+++ Trees/subtree_of_another_tree.py
@@ -6,37 +6,45 @@ class TreeNode:
         self.right = right
 
 
-# O(size of root * size of subRoot)
-class Solution:
-    def isSubtree(self, root, subRoot) -> bool:
-        def is_same_tree(root, sub_root):
-            if not root and not sub_root:
-                return True
-            if not root or not sub_root or root.val != sub_root.val:
-                return False
-            return is_same_tree(root.right, sub_root.right) and is_same_tree(
-                root.left, sub_root.left
-            )
-
-        if not subRoot:
+# DFS
+# Time: O(size of root * size of subRoot)
+# Space: O(size of root + size of subRoot)
+def isSubtree(root, subRoot) -> bool:
+    def is_same_tree(root, sub_root):
+        if not root and not sub_root:
             return True
-        if not root:
+        if not root or not sub_root or root.val != sub_root.val:
             return False
-        if is_same_tree(root, subRoot):
+        return is_same_tree(root.right, sub_root.right) and is_same_tree(
+            root.left, sub_root.left
+        )
+
+    if not subRoot:
+        return True
+    if not root:
+        return False
+    if is_same_tree(root, subRoot):
+        return True
+    return isSubtree(root.right, subRoot) or isSubtree(root.left, subRoot)
+
+# Same but using another helper function
+def isSubtree(root, subRoot) -> bool:
+    is_subtree = []
+
+    def isSameTree(p, q):
+        if not p and not q:
             return True
-        return self.isSubtree(root.right, subRoot) or self.isSubtree(root.left, subRoot)
+        if not p or not q or p.val != q.val:
+            return False
+        return isSameTree(p.right, q.right) and isSameTree(p.left, q.left)
 
+    def dfs(node):
+        nonlocal is_subtree
+        if not node:
+            return
+        is_subtree.append(isSameTree(node, subRoot))
+        dfs(node.left)
+        dfs(node.right)
 
-test = Solution()
-
-root = TreeNode(3)
-root.right = TreeNode(5)
-root.left = TreeNode(4)
-root.left.left = TreeNode(1)
-root.left.right = TreeNode(2)
-
-sub_root = TreeNode(4)
-sub_root.right = TreeNode(2)
-sub_root.left = TreeNode(1)
-
-print(test.isSubtree(root, sub_root))
+    dfs(root)
+    return any(is_subtree)
