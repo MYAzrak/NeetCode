72a8fc657850aa9740db2e1cf6ccc412494f8e3a
Mohammad Yaser Azrak <yaserazrak5@gmail.com>
2025-03-05 15:34:11 +0400

    Update "Kth Largest Element In a Stream" solution

--- Heap-PriorityQueue/kth_largest_element_in_a_stream.py
+++ Heap-PriorityQueue/kth_largest_element_in_a_stream.py
@@ -1,26 +1,17 @@
 import heapq
 
 
+# Using Min Heap if size k
+# Time: O(logn) for constructor
+# & O(nlogn) for add() since adding and popping from heaps is logn and we do (n-k) pops in the beginning
+# Space: O(n)
 class KthLargest:
     def __init__(self, k: int, nums):
         self.min_heap, self.k = nums, k
         heapq.heapify(self.min_heap)
-        while len(self.min_heap) > k: 
-            heapq.heappop(self.min_heap)  # Pops the smallest element
 
     def add(self, val: int) -> int:
         heapq.heappush(self.min_heap, val)
-        if len(self.min_heap) > self.k:
-            heapq.heappop(self.min_heap)
+        while len(self.min_heap) > self.k:
+            heapq.heappop(self.min_heap) # Pops the smallest element
         return self.min_heap[0]
-
-
-# Your KthLargest object will be instantiated and called as such:
-# obj = KthLargest(k, nums)
-# param_1 = obj.add(val)
-test = KthLargest(3, [4, 5, 8, 2])
-print(test.add(3))  # return 4
-print(test.add(5))  # return 5
-print(test.add(10))  # return 5
-print(test.add(9))  # return 8
-print(test.add(4))  # return 8
