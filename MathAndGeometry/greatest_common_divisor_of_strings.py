45de21b56d66dec8f760208e57df5d2f6e1c4a2b
Mohammad Yaser Azrak <yaserazrak5@gmail.com>
2025-03-10 16:57:20 +0400

    Solve "Greatest Common Divisor of Strings"

---
+++ MathAndGeometry/greatest_common_divisor_of_strings.py
@@ -0,0 +1,20 @@
+# Time: O()
+# Space: O()
+def gcdOfStrings(str1: str, str2: str) -> str:
+    def is_divisor(s, t):
+        return s == t * (len(s) // len(t))
+
+    gcd = min(str1, str2, key=len)
+
+    while gcd:
+        if len(str1) % len(gcd) == 0 and len(str2) % len(gcd) == 0:
+            if is_divisor(str1, gcd) and is_divisor(str2, gcd):
+                return gcd
+        gcd = gcd[:-1]
+
+    return ""
+
+
+print(gcdOfStrings(str1="ABCABC", str2="ABC"))
+print(gcdOfStrings(str1="ABABAB", str2="ABAB"))
+print(gcdOfStrings(str1="LEET", str2="CODE"))
